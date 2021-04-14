a347a4b79632f1393017eb3df98d72811a6dd340
wandji <wbertrand10@gmail.com>
2021-04-14 21:26:36 +0100

    Create contact function

--- src/index.js
+++ src/index.js
@@ -155,18 +155,45 @@ function footer(){
   const list = footer.appendChild(document.createElement('ul'));
   list.setAttribute('class', 'd-flex justify-content-around my-auto')
   const l1 = list.appendChild(document.createElement('li'))
+  l1.setAttribute('class', 'd-none d-md-inline')
   l1.textContent = 'grill_joint@email.com'
-  const l2 = list.appendChild(document.createElement('li'))
+  const l2 = list.appendChild(document.createElement('li'));
   l2.textContent = 'xxx.xxx.xxx'
-  const l4 = list.appendChild(document.createElement('li'))
-  l4.textContent = '@grill_joint'
+  const l3 = list.appendChild(document.createElement('li'))
+  l3.setAttribute('class', 'd-none d-md-inline')
+  l3.textContent = '@grill_joint'
 
   content.appendChild(footer)
 
 }
 
+function contact(){
+  const content = document.getElementById('content');
+  const section = document.createElement('section');
+  section.setAttribute('id', 'contact')
+  section.setAttribute('class', 'container-fluid remove-padding');
+
+  const contactText = section.appendChild(document.createElement('div'));
+  contactText.setAttribute('class', 'contact-text text-success my-auto ml-5 fs-1 fw-bold');
+
+  const email = contactText.appendChild(document.createElement('h4'))
+  email.textContent = 'Email: grill@email.com';
+  const phone = contactText.appendChild(document.createElement('h4'))
+  phone.textContent = 'Tel: xxx.xxx.xxx';
+  const twitter = contactText.appendChild(document.createElement('h4'))
+  twitter.textContent = 'Twitter: @grill_joint';
+  const instagram = contactText.appendChild(document.createElement('h4'))
+  instagram.textContent = 'Instagram: Mr_Grill';
+
+  content.appendChild(section);
+
+}
+
+
+
 
 header();
 // home();
 // menu();
+contact();
 footer();
