1ce7b63597944080a134056a554379831680e5e3
wandji <wbertrand10@gmail.com>
2021-04-14 22:03:58 +0100

    Add footer file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './main.scss';
 import "../assets/chicken.jpg";
 import header from './header'
 import home from './home'
+import footer from './footer'
 
 
 
@@ -94,23 +95,7 @@ function menu(){
   
 }
 
-function footer(){
-  const content = document.getElementById('content');
-  const footer = document.createElement('footer');
-  const list = footer.appendChild(document.createElement('ul'));
-  list.setAttribute('class', 'd-flex justify-content-around my-auto')
-  const l1 = list.appendChild(document.createElement('li'))
-  l1.setAttribute('class', 'd-none d-md-inline')
-  l1.textContent = 'grill_joint@email.com'
-  const l2 = list.appendChild(document.createElement('li'));
-  l2.textContent = 'xxx.xxx.xxx'
-  const l3 = list.appendChild(document.createElement('li'))
-  l3.setAttribute('class', 'd-none d-md-inline')
-  l3.textContent = '@grill_joint'
 
-  content.appendChild(footer)
-
-}
 
 function contact(){
   const content = document.getElementById('content');
