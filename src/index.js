a615000dcc712bf55f967b5131816f12b9fa8230
wandji <wbertrand10@gmail.com>
2021-04-14 21:05:20 +0100

    Create footer function

--- src/index.js
+++ src/index.js
@@ -149,7 +149,24 @@ function menu(){
   
 }
 
+function footer(){
+  const content = document.getElementById('content');
+  const footer = document.createElement('footer');
+  const list = footer.appendChild(document.createElement('ul'));
+  list.setAttribute('class', 'd-flex justify-content-around my-auto')
+  const l1 = list.appendChild(document.createElement('li'))
+  l1.textContent = 'grill_joint@email.com'
+  const l2 = list.appendChild(document.createElement('li'))
+  l2.textContent = 'xxx.xxx.xxx'
+  const l4 = list.appendChild(document.createElement('li'))
+  l4.textContent = '@grill_joint'
+
+  content.appendChild(footer)
+
+}
+
 
 header();
 // home();
-menu()
+// menu();
+footer();
