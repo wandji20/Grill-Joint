3b5b0f6907f802abc68c29dbb8ba232cb1ade051
wandji <wbertrand10@gmail.com>
2021-04-14 14:10:59 +0100

    Add menu images and menu section

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
 import './main.scss';
-import porc from "../assets/porc.jpg"
-
-const porcImg = new Image();
-porcImg.src = porc;
-porcImg.setAttribute('class', 'img-fluid')
-
-const menuText = document.querySelector('.menu-text');
-const container = document.querySelector('.container-fluid');
-const menu = document.querySelector('#menu');
-
-menu.insertBefore(porcImg, menuText );
+import "../assets/chicken.jpg";
--- webpack.config.js
+++ webpack.config.js
@@ -23,14 +23,10 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
-            },
-          },
-        ],
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
       },
     ],
 
